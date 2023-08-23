02987a8e19fcf85a1db060380b02bae47017a34d
Pedro Moretzsohn <p.moretzsohn24@gmail.com>
2023-08-23 17:13:23 -0300

    need to finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :reviews, only: :destroy
+
 end
